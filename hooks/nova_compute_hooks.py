81e9dd15ddd2c611830d2f5064220363ebfced22
Jacek Nykis <jacek.nykis@canonical.com>
2014-03-31 16:30:14 +0100

    Fixed bug in config-changed

--- hooks/nova_compute_hooks.py
+++ hooks/nova_compute_hooks.py
@@ -84,7 +84,7 @@ def config_changed():
     else:
         disable_shell(user='nova')
 
-    if config('instances-path') is None:
+    if config('instances-path') is not None:
         fp = config('instances-path')
         fix_path_ownership(fp, user='nova')
 
